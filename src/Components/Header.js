396d38a393609be39e872db270f42e745cfc0136
Khaled Almansour <Khaled@mugendai.io>
2023-02-16 13:25:08 +0300

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Container">
+      <img src="Fill 213Logo.png" alt="logo" className="logoImg"></img>
+      <h1 className="logo">my travel journal.</h1>
+    </div>
+  );
+}
+
+export default Header;
